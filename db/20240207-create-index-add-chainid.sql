077fe48cf34de678a516b59ddd35ff929c048cf9
brad <zhtxcster@gmail.com>
2024-02-07 13:45:50 +0800

    fix

--- db/20240207-create-index-add-chainid.sql
+++ db/20240207-create-index-add-chainid.sql
@@ -4,5 +4,3 @@ Use
 create index idx_tx_hash on balance_txn (tx_hash(12));
 
 ALTER TABLE block ADD chain_id BIGINT NOT NULL DEFAULT 0  COMMENT "chain id";
-
-ALTER TABLE txs ADD  chain_id BIGINT NOT NULL DEFAULT 0  COMMENT "chain id";

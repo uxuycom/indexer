79e7ccce76213cbad2888a380915c748a7e836b9
brad <zhtxcster@gmail.com>
2024-02-20 16:22:23 +0800

    code style

--- task/chain_stats_hour_task.go
+++ task/chain_stats_hour_task.go
@@ -56,13 +56,13 @@ func (t *ChainStatsTask) Exec() {
 			xylog.Logger.Infof("Exec ChainStatsTask  task!")
 			chains, _ := t.dbc.FindAllChain()
 			limit := 5000
-			for i := range chains {
+			for _, c := range chains {
 				chainStatHour := &model.ChainStatHour{}
 				chainStatHour.AddressCount = 0
 				chainStatHour.InscriptionsCount = 0
 				chainStatHour.BalanceSum = decimal.NewFromInt(0)
 
-				chainStatHour.Chain = chains[i].Chain
+				chainStatHour.Chain = c.Chain
 
 				// get current hour and next hour
 				now := time.Now()
@@ -76,29 +76,29 @@ func (t *ChainStatsTask) Exec() {
 				chainStatHour.DateHour = uint32(parseUint)
 
 				u, _ := strconv.ParseUint(lastHour.Add(-1*time.Hour).Truncate(time.Hour).Format("2006010215"), 10, 32)
-				chainStat, _ := t.dbc.FindLastChainStatHourByChainAndDateHour(chains[i].Chain, uint32(u))
+				chainStat, _ := t.dbc.FindLastChainStatHourByChainAndDateHour(c.Chain, uint32(u))
 				if chainStat == nil {
 					// first stat
 					chainStat.AddressLastId = t.cfg.Stat.AddressStartId
 					chainStat.BalanceLastId = t.cfg.Stat.BalanceStartId
-					chainStat.Chain = chains[i].Chain
+					chainStat.Chain = c.Chain
 				}
 				// address
 				addressIndex := chainStat.AddressLastId
-				address, _ := t.dbc.FindAddressTxByIdAndChainAndLimit(chainStat.Chain, addressIndex, limit)
+				addresses, _ := t.dbc.FindAddressTxByIdAndChainAndLimit(chainStat.Chain, addressIndex, limit)
 				for {
-					if len(address) > 0 {
-						for j := range address {
-							if address[j].CreatedAt.After(nowHour) {
+					if len(addresses) > 0 {
+						for _, a := range addresses {
+							if a.CreatedAt.After(nowHour) {
 								break
 							}
-							if address[j].CreatedAt.Before(nowHour) && address[j].CreatedAt.After(lastHour) {
+							if a.CreatedAt.Before(nowHour) && a.CreatedAt.After(lastHour) {
 								chainStatHour.AddressCount++
-								chainStatHour.AddressLastId = address[j].ID
+								chainStatHour.AddressLastId = a.ID
 							}
-							addressIndex = address[j].ID
+							addressIndex = a.ID
 						}
-						address, _ = t.dbc.FindAddressTxByIdAndChainAndLimit(chainStat.Chain, addressIndex, limit)
+						addresses, _ = t.dbc.FindAddressTxByIdAndChainAndLimit(chainStat.Chain, addressIndex, limit)
 					} else {
 						break
 					}
@@ -108,20 +108,20 @@ func (t *ChainStatsTask) Exec() {
 				chainStatHour.InscriptionsCount = uint32(len(inscriptions))
 				// balance
 				balanceIndex := chainStat.BalanceLastId
-				balance, _ := t.dbc.FindBalanceTxByIdAndChainAndLimit(chainStat.Chain, balanceIndex, limit)
+				balances, _ := t.dbc.FindBalanceTxByIdAndChainAndLimit(chainStat.Chain, balanceIndex, limit)
 				for {
-					if len(balance) > 0 {
-						for j := range balance {
-							if balance[j].CreatedAt.After(nowHour) {
+					if len(balances) > 0 {
+						for _, b := range balances {
+							if b.CreatedAt.After(nowHour) {
 								break
 							}
-							if balance[j].CreatedAt.Before(nowHour) && balance[j].CreatedAt.After(lastHour) {
-								chainStatHour.BalanceSum.Add(balance[j].Amount)
-								chainStatHour.BalanceLastId = balance[j].ID
+							if b.CreatedAt.Before(nowHour) && b.CreatedAt.After(lastHour) {
+								chainStatHour.BalanceSum.Add(b.Amount)
+								chainStatHour.BalanceLastId = b.ID
 							}
-							balanceIndex = balance[j].ID
+							balanceIndex = b.ID
 						}
-						balance, _ = t.dbc.FindBalanceTxByIdAndChainAndLimit(chainStat.Chain, balanceIndex, limit)
+						balances, _ = t.dbc.FindBalanceTxByIdAndChainAndLimit(chainStat.Chain, balanceIndex, limit)
 					} else {
 						break
 					}

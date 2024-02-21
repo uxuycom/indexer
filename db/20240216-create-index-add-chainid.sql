0d0323964967c8ab7c2bf06b2f36878a9e891d79
brad <zhtxcster@gmail.com>
2024-02-21 22:13:58 +0800

    add migrate sql file

--- db/20240216-create-index-add-chainid.sql
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (C) 2024 Baidu, Inc. All Rights Reserved.
- */
-Use
-tap_indexer;
-
-create index idx_tx_hash on balance_txn (tx_hash(12));
-
-ALTER TABLE block ADD chain_id BIGINT NOT NULL DEFAULT 0  COMMENT "chain id";
